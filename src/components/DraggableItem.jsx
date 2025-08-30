4b543e9c01444926bcd1c1b68eccc5b637d65b36
akshay-ken <akshaybest3120@gmail.com>
2025-08-30 22:35:59 +0530

    style: add draggable items with styling

--- src/components/DraggableItem.jsx
+++ src/components/DraggableItem.jsx
@@ -1,7 +1,22 @@
 export function DraggableItem() {
   return (
-    <section className="bg-slate-800 h-1/3 absolute inset-x-0 bottom-0">
-      <div>Item</div>
+    <section className="bg-slate-800 h-1/3 absolute inset-x-0 bottom-0 p-4 flex flex-col gap-y-6 items-center">
+      {/* draggable item one */}
+      <nav className="bg-slate-400 rounded-md w-full">
+        <ul className="flex flex-row justify-around md:text-2xl text-xl">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+      {/* draggable item two */}
+      <button className="bg-slate-400 px-6 w-fit rounded-full text-xl md:text-2xl">
+        Button
+      </button>
+      {/* draggable items three */}
+      <section className="w-full bg-slate-400 h-full flex flex-col items-center justify-center text-xl md:text-2xl">
+        HERO
+      </section>
     </section>
   );
 }
